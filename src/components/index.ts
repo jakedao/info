39be0cf07be6fcadd4b9f5f3ffd39fad1a1f369f
Khanh Dao <khanh.dao@vn.woowahan.com>
2023-11-17 16:39:53 +0700

    int cv builder

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Heading } from "./Heading";
+export { default as Section } from "./Section";
+export { default as Info } from "./Info";
+export { default as Experience } from "./Experience";
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import './index.scss';
 
 const container = document.getElementById("app") as HTMLElement;
 const root = createRoot(container);
